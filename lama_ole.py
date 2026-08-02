b14bf68440c8b42a235ac545176c5c967a2dbe0a
Frank Schwidom <schwidom@gmx.net>
2026-08-02 23:52:47 +0200

    Version

--- lama_ole.py
+++ lama_ole.py
@@ -40,7 +40,7 @@ def main():
     parser.add_argument(
         "-V", "--version",
         action="version",
-        version="0.0.30"
+        version="0.0.31"
     )
     # Define arguments
     parser.add_argument(
